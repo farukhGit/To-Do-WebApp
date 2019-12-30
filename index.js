e16e4633cc3af0d1f06c73a55e3407ed353e2d94
farukhGit <58646969+farukhGit@users.noreply.github.com>
2019-12-30 13:53:56 +0530

    First commit of ToDo app

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express'); 
+const app = express();
+const port = 8000;
+
+app.listen(port, (err)=>{
+    if(err)
+        console.log(`Error listening to port ${port}: ${err}`);  //statement uses interpolation
+    else{
+        console.log(`server is running on port ${port}`);   
+    }
+});
